aad787416a084374af22cfb26be102e79f2108e5
kouame konan abraham <abrahamkonany@gmail.com>
2023-06-26 13:07:17 +0200

    correction de dirname

--- serve.js
+++ serve.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors())
 
-
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
