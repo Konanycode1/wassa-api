d7141b1ead85f66fdd21f6b204b1f50ecd4f7160
kouame konan abraham <abrahamkonany@gmail.com>
2023-06-26 13:15:02 +0200

    correction de dirname

--- serve.js
+++ serve.js
@@ -4,13 +4,13 @@ const mongoose = require('mongoose')
 const cors = require('cors');
 const Router = require('./router/allRoute')
 const path = require('path');
-import { fileURLToPath } from 'url';
+// import { fileURLToPath } from 'url';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors())
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
